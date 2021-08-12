a03eaad83cb1909f4407a5c38964a76743fc630c
Leschonander <schonanderl@gwu.edu>
2021-08-11 23:14:19 -0400

    judicary added

--- SenateVideoScrapers/Judiciary.py
+++ SenateVideoScrapers/Judiciary.py
@@ -2,28 +2,26 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 
-# Instead of getting all at once we can just filter for the ones from today when 
-# populating whatever backend
 
-def get_homeland_security_hearings():
+def get_judiciary_hearings():
 
-    url = "https://www.hsgac.senate.gov/hearings"
+    url = "https://www.judiciary.senate.gov/hearings"
     res = requests.get(url)
 
     soup =  BeautifulSoup(res.text,'html.parser')
     table_rows = soup.findAll('tr', { 'class': 'vevent'})
     data = []
     for t in table_rows:
-        date = t.findAll("td")[0].get_text().split(" ")
 
+        date = t.findAll("td")[2].get_text().split(" ")
         row_obj = {
-            "Date": date[0],
-            "Time": date[1],
-            "URL": t.findAll("a")[0]["href"],
+            "Date": date[0].replace("\n", ""),
+            "Time": date[1].replace("\n", ""),
+            "URL": "https://www.judiciary.senate.gov/" + t.findAll("a")[0]["href"],
             "Title": t.findAll("a")[0].get_text().replace("\n", "").rstrip().replace("\t", ""),
             "Location": t.findAll("td")[2].get_text().replace("\n", "")
         }
-
+        
         data.append(row_obj)
     
     for d in data:
@@ -35,9 +33,9 @@ def get_homeland_security_hearings():
             video_url =  soup_ind.find('a', { 'id': 'watch-live-now'})["href"].replace("javascript:openVideoWin('", "").replace("');", "")
         
         d["video_url"] = video_url
-
+    
     data_table = pd.DataFrame(data)
 
     return data_table
-        
 
+get_judiciary_hearings().to_csv("ex_jud.csv")
