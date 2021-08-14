8340e6f4a9b42145c84628723190562744ef8a8f
Leschonander <schonanderl@gwu.edu>
2021-08-14 17:25:16 -0400

    agriculture added

---
+++ SenateVideoScrapers/Agriculture.py
@@ -0,0 +1,64 @@
+
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+
+def get_agricultural_hearings():
+
+    url = "https://www.agriculture.senate.gov/hearings?c=all"
+    res = requests.get(url)
+
+    soup =  BeautifulSoup(res.text,'html.parser')
+    table_rows = soup.findAll('tr', { 'class': 'vevent'})
+    data = []
+    for t in table_rows:
+        if t.find('time', {'class': 'dtstart'}) == None:
+            date = ""
+            time = ""
+        else:
+            date_time = t.find('time', {'class': 'dtstart'}).get_text().split(" ")
+            date = date_time[0]
+            time = date_time[1]
+        
+        if t.find('a', {'class': 'summary'}) == None:
+            url = ""
+            title = ""
+        else:
+            url = "https://www.agriculture.senate.gov" + t.find('a', {'class': 'summary'})["href"]
+            title = t.find('a', {'class': 'summary'}).get_text().replace("\n", "").replace("\t", "")
+        
+        if t.find('span', {'class': 'location'}) == None:
+            location = ""
+        else:
+            location =  t.find('span', {'class': 'location'}).get_text()
+        
+        row_obj = {
+            "Date": date,
+            "Time": time,
+            "URL": url,
+            "Title": title,
+            "Location": location
+        }
+        
+        data.append(row_obj)
+    
+    for d in data:
+        if d["URL"] == "":
+            d["video_url"] = ""
+        else:
+            res_ind = requests.get(d["URL"])
+            soup_ind = BeautifulSoup(res_ind.text,'html.parser')
+            
+            if soup_ind.find('a', { 'id': 'watch-live-now'}) == None:
+                video_url = ""
+            else:
+                video_url =  "https://www.agriculture.senate.gov" + soup_ind.find('a', { 'id': 'watch-live-now'})["href"].replace("javascript:openVideoWin('", "").replace("');", "")
+            
+            d["video_url"] = video_url
+    
+    data_table = pd.DataFrame(data)
+    print(data_table)
+
+    return data_table
+
+get_agricultural_hearings()
