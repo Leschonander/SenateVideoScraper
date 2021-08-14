77dbaec2915e9b7037f61d669bc10d9362d1836a
Leschonander <schonanderl@gwu.edu>
2021-08-13 21:28:38 -0400

    Finance added

---
+++ SenateVideoScrapers/Finance.py
@@ -0,0 +1,51 @@
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+
+def get_finance_hearings():
+
+    url = "https://www.finance.senate.gov/hearings?PageNum_rs=1&maxrows=999999"
+    res = requests.get(url)
+
+    soup =  BeautifulSoup(res.text,'html.parser')
+    table_rows = soup.findAll('tr', { 'class': 'vevent'})
+    data = []
+    for t in table_rows:
+        if  t.find("td", {'class': "hearing-td"}) == None:
+            link = ""
+            title = ""
+        else:
+            link = "https://www.finance.senate.gov" + t.find("td", {'class': "hearing-td"}).find("a")["href"]
+            title = t.find("td", {'class': "hearing-td"}).find("a").get_text().replace("\n", "").rstrip().replace("\t", "")
+        
+        if  t.find("time") == None:
+            date = ""
+        else:
+            date = t.find("time").get_text()
+
+        row_obj ={
+            "Date": date,
+            "Time": "",
+            "URL": link,
+            "Title": title,
+            "Location": ""
+        }
+        
+        data.append(row_obj)
+
+    for d in data:
+        res_ind = requests.get(d["URL"])
+        soup_ind = BeautifulSoup(res_ind.text,'html.parser')
+
+        if soup_ind.find('a', { 'id': 'watch-live-now'}) == None:
+            video_url = ""
+        else:
+            video_url =  soup_ind.find('a', { 'id': 'watch-live-now'})["href"].replace("javascript:openVideoWin('", "").replace("');", "")
+        
+        d["video_url"] = "https://www.finance.senate.gov"  + video_url
+    
+    data_table = pd.DataFrame(data)
+
+    return data_table
+        
+get_finance_hearings()
