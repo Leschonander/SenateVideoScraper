dce9443e4e830c07ca23865a1f7d346c464a9bf4
Leschonander <schonanderl@gwu.edu>
2021-08-17 22:03:03 -0400

    master merger of files added

---
+++ MergeSenateVideoFiles.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import os
+
+
+files = os.listdir("./SenateVideoFiles")
+data_frames = []
+for f in files:
+    file_path = "./SenateVideoFiles/" + f
+    data_frame = pd.read_csv(file_path)
+    data_frames.append(data_frame)
+
+data_frames = pd.concat(data_frames)
+data_frames.to_csv("./SenateVideoFiles/MasterFile.csv")
