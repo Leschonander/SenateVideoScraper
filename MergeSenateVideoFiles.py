2b89b7681128ffc27d1b0abb51ff7a4e8d59e15c
Leschonander <schonanderl@gwu.edu>
2021-09-16 14:36:44 -0400

    basic CLI features

--- MergeSenateVideoFiles.py
+++ MergeSenateVideoFiles.py
@@ -1,29 +1,40 @@
 import pandas as pd
+import argparse
 import os
 import time
 
-# This is the base case, that you want all files. Warning if you are 
-# setting it up for the first time, it may take sometime
 
-scripts = os.listdir("./SenateVideoScrapers")
-for s in scripts:
-    file_path = "./SenateVideoScrapers/" + s
-    os.system(f"python3 {file_path}")
-    time.sleep(1)
+parser = argparse.ArgumentParser(description='Extract data from the Senate Committee websites')
+
+parser.add_argument('Command',
+                       metavar='command',
+                       type=str,
+                       help='the specific way to extract the data')
+
+args = parser.parse_args()
+
+command = args.Command
+
+if(command == 'extract-all'):
+    scripts = os.listdir("./SenateVideoScrapers")
+    for s in scripts:
+        file_path = "./SenateVideoScrapers/" + s
+        os.system(f"python3 {file_path}")
+        time.sleep(1)
 
 
-files = os.listdir("./SenateVideoFiles")
-data_frames = []
-for f in files:
-    file_path = "./SenateVideoFiles/" + f
-    if f != "MasterFile.csv":
-        data_frame = pd.read_csv(file_path, engine='python')
-    
-        data_frames.append(data_frame)
-    else:
-        continue
+    files = os.listdir("./SenateVideoFiles")
+    data_frames = []
+    for f in files:
+        file_path = "./SenateVideoFiles/" + f
+        if f != "MasterFile.csv":
+            data_frame = pd.read_csv(file_path, engine='python')
+        
+            data_frames.append(data_frame)
+        else:
+            continue
 
-data_frames = pd.concat(data_frames)
-data_frames = data_frames[["Date","Time","URL","Title","Location","Committee","Date Scraped","video_url"]]
-print(data_frames)
-data_frames.to_csv("./SenateVideoFiles/MasterFile.csv",  encoding='utf-8', index=False)
+    data_frames = pd.concat(data_frames)
+    data_frames = data_frames[["Date","Time","URL","Title","Location","Committee","Date Scraped","video_url"]]
+    print(data_frames)
+    data_frames.to_csv("./SenateVideoFiles/MasterFile.csv",  encoding='utf-8', index=False)
--- README.md
+++ README.md
@@ -3,19 +3,21 @@
 ## About
 
 Surprisingly, there is not a central repository for past Senate Hearings across all committees. There are tables on each website for each individual
-committee, but not one that contains all the hearings. This project intends to change that. By collecting links to all the videos for various hearings, 
-it should make it easier to search for a given hearing vs navigating a given comittees website. 
+committee, but not one that contains all the hearings. This project intends to change that. By collecting links to all the videos for various hearings,
+it should make it easier to search for a given hearing vs navigating a given comittees website.
 
-Within `senateVideos`, there is a CSV file for each major Senate committee, along with `MasterFile.csv` which is a combination of all those other files. 
+Within `senateVideos`, there is a CSV file for each major Senate committee, along with `MasterFile.csv` which is a combination of all those other files.
 
 ## Requirements
 
 A `requirments.txt` file is linked in the repository. While more then 3 packages are listed, the main 3 packages
 to work with the scraper are:
 
-* Beautiful Soup
-* Pandas
-* Requests
+- Beautiful Soup
+- Pandas
+- Requests
 
-To run the scraper, the main method to get set up is to run `MergeSenateVideoFiles.py` which will run all the scrapers for the committees and then merge them into
-a master CSV file. Alternativly, you can run a individual scraper contained within `SenateVideoScrapers`.
+There are several ways to run the scraper. The main method to get set up is to run `MergeSenateVideoFiles.py` with the flag `extract-all` which will run all the scrapers for the committees and then merge them into
+a master CSV file.
+
+Alternativly, you can run a individual scraper contained within `SenateVideoScrapers`.
