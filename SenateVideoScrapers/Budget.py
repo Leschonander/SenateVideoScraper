abaa93c988dfe33b31e3fc4a5a0188c6a159050e
Leschonander <schonanderl@gwu.edu>
2021-09-16 13:50:05 -0400

    updates to rest of the 4 files

--- SenateVideoScrapers/Budget.py
+++ SenateVideoScrapers/Budget.py
@@ -22,7 +22,7 @@ def get_budget_hearings(page: int):
             date = ""
         else:
             date = t.find('time', {'class': 'dtstart'}).get_text()
-            date =  datetime.strptime(date, '%b %d %Y').strftime("%m/%d/%Y")
+            date =  datetime.strptime(date, '%m.%d.%y ').strftime("%m/%d/%Y")
         
         if t.find('a', {'class': 'summary'}) == None:
             url = ""
