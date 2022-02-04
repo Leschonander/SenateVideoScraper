8f867f5cdd66cb8e1ff8cf073af85270463129c9
Leschonander <schonanderl@gwu.edu>
2022-02-04 17:10:49 -0500

    witness data upgraded

--- SenateVideoScrapers/Finance.py
+++ SenateVideoScrapers/Finance.py
@@ -62,7 +62,7 @@ def get_finance_hearings(rows: int):
                 d["witnesses"] = ""
             else:
                 witness_html = soup_ind.findAll('span', {'class': 'fn'})
-                witness_html = [w.get_text().replace("\t", "").replace("\n", "").replace("0x80", "")  for w in witness_html]
+                witness_html = [w.get_text().replace("\t", "").replace("\n", " ").replace("0x80", "")  for w in witness_html]
                 # witness_html = [i for i in witness_html if "(" not in i]
                 witness_html = [
                     w.replace("Hon.", "")
@@ -76,11 +76,14 @@ def get_finance_hearings(rows: int):
                      .replace("Representative", "")
                      .replace("Lt", "")
                      .replace("The Honorable", "")
+                     .replace("Honorable", "")
                      .replace("Ranking Member", "")
                      .replace("Chair", "")
+                     .replace("Ambassador", "")
                      .strip() 
                     for w in witness_html
                 ]
+                witness_html = [' '.join(w.split()) for w in witness_html]
                 d["witnesses"] = witness_html
 
         print(d)
