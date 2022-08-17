14ac4e905c2ad5e9dd4a6ba962543c02cd7afa1a
Leschonander <schonanderl@gwu.edu>
2022-08-17 11:23:51 -0400

    tags for everyone

--- TagFiles.R
+++ TagFiles.R
@@ -2,10 +2,13 @@ library(tidyverse)
 
 masterfile <- read_csv("./SenateVideoFiles/MasterFile.csv")
 basic_tags <- read_csv("basic_tags_tibble.csv") %>% .[[1]]
+lda_tags <- read_csv("lda_tags.csv") %>% .[[2]]
+
+combined_tags <- c(basic_tags, lda_tags)
 
 masterfile_with_tags <- masterfile %>%
   mutate(
-    Tags = str_extract_all(Title, paste(basic_tags, collapse = "|")),
+    Tags = str_extract_all(str_to_lower(Title), paste(basic_tags, collapse="\\b|\\b")),
     Tags = map_chr(Tags, toString),
     Tags = gsub("\\b", '"', Tags, perl=T),
     Tags = map_chr(Tags, ~ str_c("[", .x, "]"))
